fa98fcb1051fef0ce436d6ed06280dbd313687e6
iamankitkuswaha <ankit21010@iiitd.ac.in>
2024-08-23 15:46:00 +0530

    Initial commit

---
+++ src/test/java/com/ankit/rating/MicroserviceRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
